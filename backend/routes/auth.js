46f803ad06329eb33f8a596791f5faa5a1fb3d4d
Abhishek N Santhosh <97347843+Abhiiofficial@users.noreply.github.com>
2023-04-12 08:47:12 +0530

    https

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -26,7 +26,7 @@ router.get("/google", passport.authenticate("google", ["profile", "email"]));
 router.get(
 	"/google/callback",
 	passport.authenticate("google", {
-		successRedirect:'https://sparkzccet.tech/register',
+		successRedirect:'http://sparkzccet.tech/register',
 		failureRedirect: "/login/failed",
 	})
 );
